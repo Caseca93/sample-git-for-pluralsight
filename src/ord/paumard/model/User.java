f7f12883a25509d1c29a0de18655284272491a3c
Caseca93 <icaseca93@gmail.com>
2022-08-24 11:45:29 -0300

    Added getter and setter

--- src/ord/paumard/model/User.java
+++ src/ord/paumard/model/User.java
@@ -2,4 +2,14 @@ package ord.paumard.model;
 
 class User {
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
